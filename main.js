6f877cd16e3aca769813340b0b7b9ed141b55202
agitatedsloth <106744648+agitatedsloth@users.noreply.github.com>
2022-06-12 21:28:25 +0100

    added all files and images

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('#button');
+let msg = document.querySelector('#message');
+
+button.addEventListener('click', (botton)=>{
+  msg.classList.toggle('reveal');
+})
+//test
